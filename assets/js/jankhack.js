6b8807f0f9ab73922cf310f7e0a9dec5a426da11
Anthony Liu <anthonyscottliu@gmail.com>
2017-04-25 23:20:51 -0400

    Make an actual splash page.

---
+++ assets/js/jankhack.js
@@ -0,0 +1,15 @@
+// jankhack.party
+// @author Anthony Liu
+// @date 2017-04-25
+
+var JankHack = (function() {
+  function initJankHack() {
+    // filler
+  }
+
+  return {
+    init: initJankHack
+  };
+})();
+
+window.addEventListener('load', JankHack.init);
